b359fec6f594dfc3aec14871beb81563fc11701c
Poomtum Rattanarat <poomtum2544@hotmail.com>
2023-04-07 21:36:55 +0700

    Initial Commit :tada:

---
+++ src/main/java/KU/BetterReciper/BetterReciperApplication.java
@@ -0,0 +1,13 @@
+package KU.BetterReciper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterReciperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterReciperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/KU/BetterReciper/BetterReciperApplicationTests.java
@@ -0,0 +1,13 @@
+package KU.BetterReciper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterReciperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
