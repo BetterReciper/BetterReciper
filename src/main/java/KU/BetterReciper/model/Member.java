208109b2921d6d07c0556a702ff4f6fd195435b3
Wha Chanunya <whachanunya@gmail.com>
2023-05-15 14:06:10 +0700

    database encryption

--- src/main/java/KU/BetterReciper/model/Member.java
+++ src/main/java/KU/BetterReciper/model/Member.java
@@ -1,19 +1,17 @@
 package KU.BetterReciper.model;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-
+    @Convert(converter = AttributeConverter.class)
     private String username;
     private String password;
+    @Convert(converter = AttributeConverter.class)
     private String firstName;
     private String lastName;
 
