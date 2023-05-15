f3b3b455d318ba55e923f2cb70ccfd2837a569b5
Jakarin-Jojo <jakarin.chujan@gmail.com>
2023-05-15 19:47:38 +0700

    Modify origins

--- src/main/java/KU/BetterReciper/controller/RecipeController.java
+++ src/main/java/KU/BetterReciper/controller/RecipeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
-@CrossOrigin(origins = "http://localhost:8080/api/recipe")
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/api/recipe")
 public class RecipeController {
