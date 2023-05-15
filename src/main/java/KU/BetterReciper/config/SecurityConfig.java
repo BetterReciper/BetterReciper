bd779ff976fc58c420fbb03812b6188b056b0f39
Jakarin-Jojo <jakarin.chujan@gmail.com>
2023-05-15 19:45:24 +0700

    Add CORS

--- src/main/java/KU/BetterReciper/config/SecurityConfig.java
+++ src/main/java/KU/BetterReciper/config/SecurityConfig.java
@@ -21,6 +21,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors()
+                .and()
                 .csrf().disable()
 
                 .authorizeRequests()
